70164a9f4fd8a456900419c223dfb624b41b10d6
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:46:30 +0300

    Create as kullanimi.sql

---
+++ as kullanimi.sql
@@ -0,0 +1,7 @@
+--AS KULLANİMİ
+--python ile ayni kullanima sahip
+--istedigin herhangi bir sutunu as kullanarak adini degiştirebilirsin
+
+--select ad as name from ogrenci
+--ad sutunun adini name olarak degistirir
+
