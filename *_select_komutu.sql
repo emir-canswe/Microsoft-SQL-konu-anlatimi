cfc4a76ffac19a249e98398bf7c10f5e974bbb3d
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:58:28 +0300

    Create *_select_komutu.sql

---
+++ *_select_komutu.sql
@@ -0,0 +1,4 @@
+SELECT * FROM ogrenci  --ogrenci tablosundaki tumd degerleri yazmana yardimci olur
+--* senin tum verileri cekmeni saglar
+-- yildiz yerine diger isimleri yazarsan diger verileri cekmene yardimci olur
+--yani istedigin seyi cekmek istiyorsan yanina yaz
