9457186abdce86c6b2f66830f4c09fc29f740245
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:59:53 +0300

    Update where_komutu.sql

--- where_komutu.sql
+++ where_komutu.sql
@@ -2,3 +2,6 @@ SELECT *FROM ogrenci WHERE erkekmi=1
 --bu ise senin if gibi sadece erkek olanlari yazmani saglar
 --yani istedigini ad='emir' yaptiginsa sana sadece emir ismindeki kisilerin adlarini yazmani saglar
 --istersem buyuktur kucuktur ve işlemlerini de yapabilirsin
+SELECT *FROM ogrenci WHERE okulno>100 AND erkekmi=1
+--bu da senin girdigin sarti sana yazdir farkli bir sey yok
+--normal python gibi dusun
