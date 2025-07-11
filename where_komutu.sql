bf85ad4259a222c29ec6b7f148009cd332194e48
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:59:25 +0300

    Create where_komutu.sql

---
+++ where_komutu.sql
@@ -0,0 +1,4 @@
+SELECT *FROM ogrenci WHERE erkekmi=1
+--bu ise senin if gibi sadece erkek olanlari yazmani saglar
+--yani istedigini ad='emir' yaptiginsa sana sadece emir ismindeki kisilerin adlarini yazmani saglar
+--istersem buyuktur kucuktur ve işlemlerini de yapabilirsin
