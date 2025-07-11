f13b23ade847e422ef21b5525c3c5206b437830c
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:52:28 +0300

    Create kod_ile_tabloya_deger_gonderme.sql

---
+++ kod_ile_tabloya_deger_gonderme.sql
@@ -0,0 +1,4 @@
+INSERT INTO ogrenci
+VALUES ('ozge', 'kaya', 11, '02345465765', 0, '2009-10-10', '2003-09-02', 100, 50, 60);
+--bu kodlar sana kod olarak tabloya deger atamwsi yapmani saglar
+--bir INSERT INTO İLE deger atamasni yapacaksin
