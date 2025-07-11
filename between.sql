1daa3add6351403e8c301765e238093464a51f26
emir-canswe <canemircan38@gmail.com>
2025-07-11 14:47:37 +0300

    Create between.sql

---
+++ between.sql
@@ -0,0 +1,11 @@
+--BETWEEN KULLANMİ
+
+--SELECT * from ogrenci where okulno between 10 and 500
+--okul numarasi 10 ile 500 arasindaki olanlari sana yazmani saglar
+
+--select * from ogrenci
+
+select * from ogrenci where sinav2 between 10 and 70
+--sana sinav notu 10 ile 70 arasindaki kisileri verir
+
+--yani istedigini burda yazabilrisin
